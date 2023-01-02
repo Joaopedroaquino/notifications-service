f9b76e3fcf8cfe1d967b3eda332ab16eb39e8387
Joao Pedro Aquino <aqerhack@gmail.com>
2023-01-02 20:25:35 -0300

    create user entity

---
+++ src/users/database/user.entity.ts
@@ -0,0 +1,14 @@
+import { Column, PrimaryGeneratedColumn } from "typeorm"
+
+export class UserEntity {
+    @PrimaryGeneratedColumn()
+    id: number
+    @Column()
+    name: string
+    @Column()
+    email: string
+    @Column()
+    phone: string
+    @Column()
+    password: string
+}
