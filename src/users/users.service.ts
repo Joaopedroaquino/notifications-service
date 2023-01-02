af5bf7e5c18e078d0c47ff4d05a34a4cad976e97
Joao Pedro Aquino <aqerhack@gmail.com>
2023-01-02 20:51:50 -0300

    refactor userService

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -6,9 +6,10 @@ import { User } from './interfaces/user.interface';
 
 @Injectable()
 export class UsersService {
-    constructor(@InjectRepository(UserEntity)private userRepository: Repository<UserEntity>){}
-    private users: User[] = [];
-  async  create(user: User): Promise <UserEntity> {
+    constructor(@InjectRepository(UserEntity)
+    private userRepository: Repository<UserEntity>){}
+
+    async  create(user: User): Promise <UserEntity> {
        
         return  await this.userRepository.save(user)
     }
