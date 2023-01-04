13dac1303c6ee00eb69f670251df108297699bd6
joaopedroaquino <aqerhack@gmail.com>
2023-01-04 09:07:35 -0300

    :bulb: Add Swagger doc in UsersController :bulb:

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,11 @@
 import { Body, Controller, Get, OnModuleInit, Post } from '@nestjs/common';
 import { Client, ClientKafka, Transport } from '@nestjs/microservices';
-import { ApiBody } from '@nestjs/swagger';
+import { ApiBody, ApiTags } from '@nestjs/swagger';
 import { Observable } from 'rxjs';
 import { UserDto } from './dtos/user.dto';
 import { User } from './interfaces/user.interface';
 
+@ApiTags('User')
 @Controller('users')
 export class UsersController implements OnModuleInit {
     @Client({
